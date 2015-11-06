839694afd23f3bc46ddb0a8e7a0d741afd7af251
brandly <brandly13@gmail.com>
2015-11-06 01:08:04 -0500

    get magnet from req.params

--- src/server/app.js
+++ src/server/app.js
@@ -5,9 +5,8 @@ import s3 from './modules/s3'
 const app = express()
 const client = new WebTorrent()
 
-var magnet = 'magnet:?xt=urn:btih:2b12ce09236526a728c6974c0d89d52860e82daa&dn=Major+Lazer+x+DJ+Snake+feat.+M%26Oslash%3B+-+Lean+On.mp3&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'
-
 app.get ('/', function(req, res) {
+  const { magnet } = req.params
   client.add(magnet, function (torrent) {
     torrent.files.forEach((file) => {
       var read = file.createReadStream()
